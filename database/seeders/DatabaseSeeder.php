82098b2b70593d4289f622f86a9fc64530fa16c4
Lukas Hajda <lukas.hajda@demi.sk>
2024-04-11 10:24:23 +0200

    Auth fixies (auth)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Testing\Fluent\Concerns\Has;
 
 class DatabaseSeeder extends Seeder
 {
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function () {
     return ['ff' => 1];
 })->middleware('auth:sanctum');
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
