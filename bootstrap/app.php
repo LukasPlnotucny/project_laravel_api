df9c84621179e497da5644dc36864de6c2b2eeff
Lukas Hajda <lukas.hajda@demi.sk>
2024-04-19 11:00:34 +0200

    Edit password, Auth (reset-password)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->group('api', [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
